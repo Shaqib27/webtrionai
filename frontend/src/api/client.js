69661e3dd024427a890ea4e850bab53722c7eabd
md shaqib hussain <mdshaqibhussain@mds-MacBook-Air.local>
2026-03-01 21:44:44 +0530

    link backend to fronted

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8001",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 // 🔐 Attach token automatically to every request
